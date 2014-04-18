c1afad06b1d83a71cc35cc8185fcd8700566f61f
Travis <maxtiff@users.noreply.github.com>
2014-04-18 11:52:19 -0500

    Update corr.R

--- corr.R
+++ corr.R
@@ -8,22 +8,32 @@ corr <- function(directory, threshold = 0) {
   ## nitrate and sulfate; the default is 0
   
   ## Return a numeric vector of correlations
+  
+  # Set files and correlation matrices
   files <- dir(directory)
+  corr_data <- numeric(0)
   
   for (i in seq_along(files)) {
-    
+    # Assign number of complete obs in table to variable to check against threshold.
     obs <- complete(directory, i)$nobs
-    print(obs)
     
+    # Check amount of completed obs in table against threshold
     if (obs > threshold) {
       
+      # Open file
       doc <- files[i]
       path <- paste(directory, doc, sep="/")
-      data <- read.csv(path)
+      data <- read.csv(path, header=TRUE)
       
-      keep <- complete.cases(data)
-      print(keep[data])
+      # Determine correlation between sulfate and nitrate
+      corr_entry <- cor(data$sulfate, data$nitrate, use="complete.obs")
+      
+      # Add correlation figure to correlation vector.
+      corr_data <- c(corr_data, corr_entry)
     }
+    
   }
+  
+  return(corr_data)
   
 }
